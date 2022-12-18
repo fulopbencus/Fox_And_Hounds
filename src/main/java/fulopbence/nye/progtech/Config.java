d484d2f6eddbf6d98f3a8a4042803c2aff4d8aa8
Fülöp Bence <fulopbencus@gmail.com>
2022-12-19 00:55:32 +0100

    Spring

---
+++ src/main/java/fulopbence/nye/progtech/Config.java
@@ -0,0 +1,125 @@
+package fulopbence.nye.progtech;
+
+import fulopbence.nye.progtech.model.GameState;
+import fulopbence.nye.progtech.model.MapVo;
+import fulopbence.nye.progtech.service.command.Command;
+import fulopbence.nye.progtech.service.command.FoxPutPerformer;
+import fulopbence.nye.progtech.service.command.HoundMove;
+import fulopbence.nye.progtech.service.command.InputHandler;
+import fulopbence.nye.progtech.service.command.impl.*;
+import fulopbence.nye.progtech.service.game.GameController;
+import fulopbence.nye.progtech.service.game.GameStepPerformer;
+import fulopbence.nye.progtech.service.input.UserInputReader;
+import fulopbence.nye.progtech.service.map.MapReaderFacade;
+import fulopbence.nye.progtech.service.map.parser.MapParser;
+import fulopbence.nye.progtech.service.map.validation.reader.MapReader;
+import fulopbence.nye.progtech.service.map.validation.reader.impl.BufferedReaderMapReader;
+import fulopbence.nye.progtech.ui.MapPrinter;
+import fulopbence.nye.progtech.ui.MapUtil;
+import fulopbence.nye.progtech.ui.PrintWrapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+/**
+ * Configurator.
+ */
+@Configuration
+public class Config {
+
+    @Bean
+    GameState gameState () {
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("map/staticmap.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        MapReader mapReader = new BufferedReaderMapReader(reader);
+        MapParser mapParser = new MapParser(8, 8);
+        MapReaderFacade mapReaderFacade = new MapReaderFacade(mapReader, mapParser);
+        MapVo mapVo = mapReaderFacade.readMap();
+        return new GameState(mapVo, false);
+    }
+    @Bean
+    Command printCommand(){
+        return new PrintCommand(mapPrinter(mapUtil(), printWrapper()), gameState());
+    }
+    @Bean
+    Command exitCommand(){
+        return new ExitCommand(gameState());
+    }
+    @Bean
+    Command foxDownLeftCommand(){
+        return new FoxDownLeftCommand(gameState(), foxPutPerformer(),mapPrinter(mapUtil(), printWrapper()),printWrapper(),gameState().getMapVo(),houndMove());
+    }
+    @Bean
+    Command foxDownRightCommand(){
+        return new FoxDownRightCommand(gameState(), foxPutPerformer(),mapPrinter(mapUtil(), printWrapper()),printWrapper(),gameState().getMapVo(),houndMove());
+    }
+    @Bean
+    Command foxUpLeftCommand(){
+        return new FoxUpLeftCommand(gameState(), foxPutPerformer(),mapPrinter(mapUtil(), printWrapper()),printWrapper(),gameState().getMapVo(),houndMove());
+    }
+    @Bean
+    Command foxUpRightCommand(){
+        return new FoxUpRightCommand(gameState(), foxPutPerformer(),mapPrinter(mapUtil(), printWrapper()),printWrapper(),gameState().getMapVo(),houndMove());
+    }
+    @Bean
+    Command defaultCommand(){
+        return new DefaultCommand(printWrapper());
+    }
+    @Bean
+    InputHandler inputHandler() {
+        return new InputHandler(List.of(
+                printCommand(),
+                exitCommand(),
+                foxDownLeftCommand(),
+                foxDownRightCommand(),
+                foxUpLeftCommand(),
+                foxUpRightCommand(),
+                defaultCommand()
+        ));
+    }
+    @Bean
+    MapUtil mapUtil(){
+        return new MapUtil();
+    }
+    @Bean
+    GameStepPerformer gameStepPerformer(UserInputReader userInputReader, InputHandler inputHandler) {
+        return new GameStepPerformer(userInputReader, inputHandler);
+    }
+
+    @Bean
+    GameController gameController(GameState gameState, MapUtil mapUtil, GameStepPerformer gameStepPerformer){
+        return new GameController(gameState, mapUtil, gameStepPerformer);
+    }
+
+    @Bean
+    UserInputReader userInputReader() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return new UserInputReader(bufferedReader);
+    }
+
+    @Bean
+    MapPrinter mapPrinter(MapUtil mapUtil, PrintWrapper printWrapper) {
+        return new MapPrinter(8, 8, mapUtil, printWrapper);
+    }
+
+    @Bean
+    FoxPutPerformer foxPutPerformer() {
+        return new FoxPutPerformer();
+    }
+
+    @Bean
+    HoundMove houndMove(){
+        return new HoundMove();
+    }
+
+    @Bean
+    PrintWrapper printWrapper() {
+        return new PrintWrapper();
+    }
+
+}
--- src/main/java/fulopbence/nye/progtech/Main.java
+++ src/main/java/fulopbence/nye/progtech/Main.java
@@ -11,7 +11,6 @@ import fulopbence.nye.progtech.model.MapVo;
 import fulopbence.nye.progtech.service.command.Command;
 import fulopbence.nye.progtech.service.command.FoxPutPerformer;
 import fulopbence.nye.progtech.service.command.HoundMove;
-import fulopbence.nye.progtech.service.command.HoundMove;
 import fulopbence.nye.progtech.service.command.InputHandler;
 import fulopbence.nye.progtech.service.command.impl.*;
 import fulopbence.nye.progtech.service.game.GameController;
@@ -24,9 +23,11 @@ import fulopbence.nye.progtech.service.map.validation.reader.impl.BufferedReader
 import fulopbence.nye.progtech.ui.MapPrinter;
 import fulopbence.nye.progtech.ui.MapUtil;
 import fulopbence.nye.progtech.ui.PrintWrapper;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
- * Main class. Where it's all connected.
+ * Main class.
  */
 public class Main {
     /**
@@ -34,43 +35,8 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("map/staticmap.txt");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-        MapReader mapReader = new BufferedReaderMapReader(reader);
-
-        MapParser mapParser = new MapParser(8, 8);
-        MapUtil mapUtil = new MapUtil();
-
-        MapReaderFacade mapReaderFacade = new MapReaderFacade(mapReader, mapParser);
-        MapVo mapVo = mapReaderFacade.readMap();
-
-        FoxPutPerformer foxputPerformer = new FoxPutPerformer();
-        HoundMove houndMove = new HoundMove();
-
-        GameState gameState = new GameState(mapVo, false);
-
-        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-        UserInputReader userInputReader = new UserInputReader(stdin);
-
-        PrintWrapper printWrapper = new PrintWrapper();
-        MapPrinter mapPrinter = new MapPrinter(8, 8, mapUtil, printWrapper);
-
-        List<Command> commandList = Arrays.asList(
-                new PrintCommand(mapPrinter, gameState),
-                new ExitCommand(gameState),
-                new FoxDownLeftCommand(gameState, foxputPerformer, mapPrinter, printWrapper, mapVo, houndMove),
-                new FoxDownRightCommand(gameState, foxputPerformer, mapPrinter, printWrapper, mapVo, houndMove),
-                new FoxUpRightCommand(gameState, foxputPerformer, mapPrinter, printWrapper, mapVo, houndMove),
-                new FoxUpLeftCommand(gameState, foxputPerformer, mapPrinter, printWrapper, mapVo, houndMove),
-                new DefaultCommand(printWrapper)
-        );
-
-        InputHandler inputHandler = new InputHandler(commandList);
-
-        GameStepPerformer gameStepPerformer = new GameStepPerformer(userInputReader, inputHandler);
-
-        GameController gameController = new GameController(gameState, mapUtil, gameStepPerformer);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("fulopbence.nye.progtech");
+        GameController gameController = applicationContext.getBean(GameController.class);
         gameController.gameLoop();
     }
 
